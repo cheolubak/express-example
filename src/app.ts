b8c285431a0f0cba817e6232eb46e81e87dc0ec3
Cheolu.B <dev.cheolubak@gmail.com>
2022-11-09 20:13:23 +0900

    init

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+app.get('/welcome', (req: Request, res: Response, next: NextFunction) => {
+  res.send('welcome!');
+});
+
+app.listen('3000', () => {
+  console.log('Server listening on port : 3000');
+});
